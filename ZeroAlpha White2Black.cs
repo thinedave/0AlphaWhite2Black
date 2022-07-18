c3ebc6efb1f953c4ef3fb82b045d2761b5bdca59
thinedave <94513260+thinedave@users.noreply.github.com>
2022-07-17 17:35:20 -0700

    Add files via upload

---
+++ ZeroAlpha White2Black.cs
@@ -0,0 +1,28 @@
+
+void Render(Surface dst, Surface src, Rectangle rect)
+{
+    Rectangle selection = EnvironmentParameters.SelectionBounds;
+
+    ColorBgra currentPixel;
+    for (int y = rect.Top; y < rect.Bottom; y++)
+    {
+        if (IsCancelRequested) return;
+        for (int x = rect.Left; x < rect.Right; x++)
+        {
+            currentPixel = src[x,y];
+
+            if (currentPixel.R == 255 &&
+            currentPixel.G == 255 &&
+            currentPixel.B == 255 &&
+            currentPixel.A == 0) {
+                currentPixel.R = 0;
+                currentPixel.G = 0;
+                currentPixel.B = 0;
+                currentPixel.A = 0;
+
+            }
+
+            dst[x,y] = currentPixel;
+        }
+    }
+}
